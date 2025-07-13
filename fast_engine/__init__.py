957437e7b4b4560af90e53da516cef7579779c99
Federico Monfasani <67231410+fmonfasani@users.noreply.github.com>
2025-07-13 00:45:17 -0300

    Update __init__.py

--- fast_engine/__init__.py
+++ fast_engine/__init__.py
@@ -1,5 +1,5 @@
 """fast-engine package."""
 __version__ = "0.1.0"
-__all__ =
+__all__ = ["main", "__version__"]  # Add other public objects as needed
 def main():
     return "fast-engine works" ["cli"]
