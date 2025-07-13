852bdc402beef59703c27daa042176597fba5d0a
Federico Monfasani <67231410+fmonfasani@users.noreply.github.com>
2025-07-13 00:32:29 -0300

    Add basic package, test, and CI

---
+++ fast_engine/__init__.py
@@ -0,0 +1,7 @@
+"""Fast Engine package."""
+
+__all__ = ["main"]
+
+def main():
+    """Return a message indicating that the engine is running."""
+    return "fast-engine works"
---
+++ tests/test_fast_engine.py
@@ -0,0 +1,5 @@
+import fast_engine
+
+
+def test_main():
+    assert fast_engine.main() == "fast-engine works"
